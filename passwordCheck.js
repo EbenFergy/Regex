2c26efc1a5056617090b798e526ebb374dfdf444
Osas Ferguson <osasferguson@Osass-MacBook-Pro.local>
2022-08-29 05:04:12 -0400

    completed the password Regex

--- passwordCheck.js
+++ passwordCheck.js
@@ -1,5 +1,12 @@
+// password must be six characters or more
+// password must contatin characters
+// must conain numbers
+// must contain upper and lower case letters
 const passwordCheck = (password) => {
   const regex =
-    /(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[\!@#\$%&\^&\*\(\)\-_\+=\\\}\{\]\["';:\/\?><\.,`~]+)/g;
+    /(?=.*[a-z]+)(?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[\!@#\$%&\^&\*\(\)\-_\+=\\\}\{\]\["';:\/\?><\.,`~]+){6,}/g;
+  console.log(regex.test(password));
   return regex.test(password);
 };
+
+passwordCheck("d8Dd8D5%");
